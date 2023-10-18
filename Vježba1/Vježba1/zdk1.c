6570e04d2277819e067f80c6e4699584d8d8c622
Karelo2705 <karlo.blaskovic@gmail.com>
2023-10-18 17:24:14 +0200

    Finalized program

--- Vježba1/Vježba1/zdk1.c
+++ Vježba1/Vježba1/zdk1.c
@@ -14,24 +14,42 @@ typedef struct {
 	double points;
 }student;
 
+void studPrint(student* stud, int numStud);
+void loadDat(FILE* fp, student* stud);
+
 int main() {
 	FILE* file = NULL;
 	int studNum = NULL;
 	studNum = numOfStudents(file);
-	printf("%d", studNum);
-
-	
-
+	student* stud = (student*)malloc(studNum*sizeof(student));
+	loadDat(file, stud, studNum);
+	studPrint(stud, studNum);
 }
 
 int numOfStudents(FILE* fp) {
 	int counter = 1;
 	char a;
 	fp = fopen("Students.txt", "r");
-	for (a = getc(fp); a != EOF; a = getc(fp))
-		if (a == '\n')
+	for (a = getc(fp); a != EOF; a = getc(fp)) {
+		if (a == '\n') {
 			counter = counter + 1;
+		}
+	}
 	fclose(fp);
 	return counter;
 }
+ 
+void loadDat(FILE* fp, student* stud, int numStud) {
+	int i;
+	fp = fopen("Students.txt", "r");
+	for (i = 0; i < numStud; i++) {
+		fscanf(fp, "%s %s %d \n", (stud + i)->name, (stud + i)->surname, &((stud + i)->points));
+	}
+	fclose(fp);
 
+}
+
+void studPrint(student* stud, int numStud) {
+	for(int i=0;i<numStud;i++)
+	printf("Ime: %s\n Prezime: %s\n Apsolutni bodovi: %d\n Relativni bodovi: %d\n\n",(stud+i)->name,(stud+i)->surname, (stud+i)->points, ((stud + i)->points)/MAX_POINTS*100);
+}
