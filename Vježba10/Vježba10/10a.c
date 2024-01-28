d6059aeb08dfcbdd0c6f160e3d896cfb25e01a3a
Karelo2705 <karlo.blaskovic@gmail.com>
2024-01-28 17:36:50 +0100

    added missing tasks

--- Vježba10/Vježba10/10a.c
+++ Vježba10/Vježba10/10a.c
@@ -1,9 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define TABLE_SIZE 11
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 struct cities;
 typedef struct cities* Tree;
@@ -12,7 +11,7 @@ typedef struct cities {
     Tree R;
     Tree L;
     int residents;
-} Cities;
+}Cities;
 
 struct countries;
 typedef struct countries* Position;
@@ -20,84 +19,62 @@ typedef struct countries {
     char name[50];
     Tree city;
     Position next;
-} Countries;
+}Countries;
 
-typedef struct {
-    Position table[TABLE_SIZE];
-} HashTable;
-
-unsigned int hash(char* key);
-void initializeTable(HashTable* table);
-int AddCountry(HashTable* table, char* NameOfCountry, char* file);
+int AddCountry(char* NameOfCountry, char* file, Position P);
 int AddCity(char* file, Tree S);
-Tree EnterCity(char* name, int residents, Tree S);
-int PrintCountries(HashTable* table);
+Tree EnterCity(char* ime, int brojstan, Tree S);
+int PrintCountries(Position P);
 int PrintCities(Tree S);
-Position SearchCountry(HashTable* table, char* name);
-int SearchCity(Tree S, int searched_number);
+Position SearchCountry(Position P, char* ime);
+int SearchCity(Tree S, int brojst);
 
 int main()
 {
-    HashTable table;
-    initializeTable(&table);
-
+    Countries Head = { .name = "", .city = NULL, .next = NULL };
     char NameOfCountry[50];
     char NameOfFile[50];
+    Position P = NULL;
     int residents = 0;
+    FILE* f = NULL;
 
-    FILE* f = fopen("drzave.txt", "r");
+    f = fopen("drzave.txt", "r");
     if (f == NULL)
     {
         printf("ERROR: File cannot be opened.\n");
         return -1;
     }
-
     while (!feof(f))
     {
         fscanf(f, "%s %s", NameOfCountry, NameOfFile);
-        AddCountry(&table, NameOfCountry, NameOfFile);
+        AddCountry(NameOfCountry, NameOfFile, &Head);
     }
     fclose(f);
-
-    PrintCountries(&table);
+    PrintCountries(Head.next);
 
     printf("\nEnter the name of the wanted country: ");
     scanf("%s", NameOfCountry);
-
-    Position P = SearchCountry(&table, NameOfCountry);
+    P = SearchCountry(Head.next, NameOfCountry);
     if (P == NULL)
     {
         printf("This country doesn't exist in the current file, please try again.\n");
         return 0;
     }
-
     printf("Minimum population: ");
     scanf(" %d", &residents);
-
     printf("\nCities of %s with more than %d inhabitants:\n", P->name, residents);
     SearchCity(P->city, residents);
 
     return 0;
 }
 
-unsigned int hash(char* key) {
-    unsigned int sum = 0;
-    for (int i = 0; i < 5 && key[i] != '\0'; ++i) {
-        sum += key[i];
-    }
-    return sum % TABLE_SIZE;
-}
+int AddCountry(char* NameOfCountry, char* file, Position P)
+{
+    Position q = NULL;
+    Position prev = NULL;
+    Tree c = NULL;
 
-void initializeTable(HashTable* table) {
-    for (int i = 0; i < TABLE_SIZE; ++i) {
-        table->table[i] = NULL;
-    }
-}
-
-int AddCountry(HashTable* table, char* NameOfCountry, char* file) {
-    unsigned int index = hash(NameOfCountry);
-
-    Position q = (Position)malloc(sizeof(Countries));
+    q = (Position)malloc(sizeof(Countries));
     if (q == NULL)
     {
         printf("Memory allocation unsuccessful.\n");
@@ -105,45 +82,60 @@ int AddCountry(HashTable* table, char* NameOfCountry, char* file) {
     }
 
     strcpy(q->name, NameOfCountry);
-    q->city = NULL;
-    q->next = table->table[index];
-    table->table[index] = q;
+    q->city = c;
 
-    q->city = (Tree)malloc(sizeof(Cities));
-    if (q->city == NULL)
+    c = (Tree)malloc(sizeof(Cities));
+    if (c == NULL)
     {
         printf("Memory allocation unsuccessful.\n");
         return -1;
     }
 
-    strcpy(q->city->name, "");
-    q->city->residents = 0;
-    q->city->R = NULL;
-    q->city->L = NULL;
+    strcpy(c->name, "");
+    c->residents = 0;
+    c->R = NULL;
+    c->L = NULL;
+
+    q->city = c;
 
     AddCity(file, q->city);
 
+    prev = P;
+    while (P != NULL)
+    {
+        if (P->next == NULL)
+        {
+            P->next = q;
+            q->next = NULL;
+            break;
+        }
+        if (strcmp(q->name, prev->next->name) < 0)
+        {
+            q->next = prev->next;
+            prev->next = q;
+            break;
+        }
+        P = P->next;
+    }
     return 0;
 }
 
 int AddCity(char* file, Tree S)
 {
-    FILE* f = fopen(file, "r");
-    if (f == NULL)
-    {
-        printf("ERROR: File cannot be opened.\n");
-        return -1;
-    }
-
+    FILE* f = NULL;
     char name[50];
     int residents = 0;
 
+    f = fopen(file, "r");
+    if (f == NULL)
+    {
+        printf("ERROR: File cannot be opened.\n"); return -1;
+    }
     while (!feof(f))
     {
         fscanf(f, "%s %d", name, &residents);
         S = EnterCity(name, residents, S);
     }
-
     fclose(f);
     return 0;
 }
@@ -176,23 +168,20 @@ Tree EnterCity(char* name, int residents, Tree S)
             S->R = EnterCity(name, residents, S->R);
         else
             S->L = EnterCity(name, residents, S->L);
+
     }
 
     return S;
 }
 
-int PrintCountries(HashTable* table)
+int PrintCountries(Position P)
 {
     printf("COUNTRIES:\n");
-    for (int i = 0; i < TABLE_SIZE; ++i)
+    while (P != NULL)
     {
-        Position P = table->table[i];
-        while (P != NULL)
-        {
-            printf("\n%s:\n", P->name);
-            PrintCities(P->city->R);
-            P = P->next;
-        }
+        printf("\n%s:\n", P->name);
+        PrintCities(P->city->R);
+        P = P->next;
     }
     return 0;
 }
@@ -208,15 +197,13 @@ int PrintCities(Tree S)
     return 0;
 }
 
-Position SearchCountry(HashTable* table, char* name)
+Position SearchCountry(Position P, char* name)
 {
-    unsigned int index = hash(name);
 
-    Position P = table->table[index];
+
     while (P != NULL)
     {
-        if (strcmp(P->name, name) == 0)
-        {
+        if (strcmp(P->name, name) == 0) {
             return P;
         }
         P = P->next;
@@ -227,8 +214,7 @@ Position SearchCountry(HashTable* table, char* name)
 
 int SearchCity(Tree S, int searched_number)
 {
-    if (S == NULL)
-    {
+    if (S == NULL) {
         return 0;
     }
     SearchCity(S->R, searched_number);
